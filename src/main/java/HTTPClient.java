699d0f8def241988c7935c4d0f8ca7a1d71eb6b4
David <davidhaldorsen@gmail.com>
2022-09-13 12:51:18 +0200

    First part done, getting initial http request to work

--- src/main/java/HTTPClient.java
+++ src/main/java/HTTPClient.java
@@ -1,4 +1,23 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class HTTPClient {
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("httpbin.org", 80);
+        String request =
+                "GET /html HTTP/1.1\r\n" +
+                "Host: httpbin.org\r\n" +
+                "Connection: close\r\n" +
+                "\r\n";
+        socket.getOutputStream().write(request.getBytes());
+
+        int c;
+        while ((c = socket.getInputStream().read()) != -1){
+            System.out.print((char)c);
+        }
+
+    }
 
     public int getResponse() {
         return 200;
