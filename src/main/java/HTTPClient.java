b389a9a915b5510fc9b4422f359bdc43d7b5046c
Arnold <arnold.gonczlik@hotmail.com>
2022-09-27 12:32:27 +0200

    Changed the way it reads respose headers and parses them, now ready to create fuction to read body with content-length

--- src/main/java/HTTPClient.java
+++ src/main/java/HTTPClient.java
@@ -1,12 +1,14 @@
-import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.util.Locale;
+import java.util.*;
 
 public class HTTPClient {
 
     String response;
+
+    String allHeaders;
 
     public HTTPClient(String hostname, int port, String path) throws IOException {
         Socket socket = new Socket(hostname, port);
@@ -15,33 +17,68 @@ public class HTTPClient {
                         "Host: " + hostname + "\r\n" +
                         "Connection: close\r\n" +
                         "\r\n";
+
         socket.getOutputStream().write(request.getBytes());
-        
-        String response = getHeaders(socket);
 
-        var test = socket.getInputStream().read();
+        //Parses the data to only get the header parts
+        allHeaders = getResponse(socket);
+        System.out.println("");
 
-        int c;
-        while ((c = socket.getInputStream().read()) != -1){
-            System.out.print((char)c);
-            response += (char)c;
-        }
-        System.out.println("Done writing");
+        var test = getOnlyHeaders(allHeaders);
+
+        //Split entire text into header and body
+        //allHeaders = new String(buffer.readAllBytes(), StandardCharsets.UTF_8).split("\\r\\n\\r\\n")[0];
+
+
+        //TODO now make it get content length and read entire body with correct encoding
     }
 
-    private static String getHeaders(Socket socket) throws IOException {
-        ByteArrayInputStream buffer = new ByteArrayInputStream(socket.getInputStream().readAllBytes());
+
+    //TODO get it to read headers in another way, so that we know how much to read of body in content-length
+
+    //Read until there is \r\n\r\n in string and return properly UTF formatted string of headers
+    public String getResponse(Socket socket) throws IOException {
+        String newlineCheck = "";
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
         int c;
-        String s = buffer.toString(StandardCharsets.UTF_8);
-        while ((c = socket.getInputStream().read()) != '\r')) {
-            buffer
+
+        while (!(newlineCheck.contains("\r\n\r\n"))) {
+            c = socket.getInputStream().read();
+            newlineCheck += (char)(c);
+            baos.write(c);
         }
 
-        return "";
+        return baos.toString(StandardCharsets.UTF_8);
+    }
+
+    private static HashMap<String, String> getOnlyHeaders(String text) {
+        var headerStore = new HashMap<String, String>();
+        //var lineSplit = "";
+        //Use regex and add all lines that match the header regex
+        for (String line: new ArrayList<>(Arrays.asList(text.split("\\r\\n")))
+             ) {
+            if (line.matches(".+:(\\s)?.+")) {
+                var lineSplit = line.split(":(\\s)?", 2);
+                headerStore.put(lineSplit[0].toLowerCase(), lineSplit[1].toLowerCase());
+            }
+        }
+        System.out.println("");
+
+        return headerStore;
+    }
+
+    public String getSpecificHeader(String specifiedHeader) {
+        var headerStore = getOnlyHeaders(allHeaders);
+
+        try {
+            return headerStore.get(specifiedHeader.toLowerCase());
+        } catch (Exception e){
+            return "Not found";
+        }
     }
 
     public int getStatusCode() {
-        String firstLineOfResponse = response.split("\r\n")[0];
+        String firstLineOfResponse = response;
         String responseCode = firstLineOfResponse.split(" ")[1];
 
         return Integer.parseInt(responseCode);
